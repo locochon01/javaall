63d9bab7c073dcdfd064524635eb90bd164bfd50
JungOh <twelve727@gmail.com>
2022-12-14 14:24:03 +0900

    05_array2

--- 04_array/src/edu/kh/array/practice/ArrayPractice.java
+++ 04_array/src/edu/kh/array/practice/ArrayPractice.java
@@ -507,3 +507,42 @@ public class ArrayPractice {
         System.out.println(Arrays.toString(arr));
     }
 }
+
+/* Scanner sc = new Scanner(System.in)
+ * int arr[] = new int[5];
+
+	for (int i = 0; i < arr.length; i++) {
+		System.out.println("정수 입력 : " , i);
+		arr[i] = sc.nextInt();
+		}
+		
+		System.out.print("검색할 값 : ")
+		int search = sc.nextInt();
+		
+		boolean flag = true;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if ( arr[i] == search) {
+			System.out.println("인덱스 : " + i);
+			flag = false;
+			break;
+			
+			if (flag) System.out.println("일치값이 존재하지 않습니다.");
+			}
+		}
+			
+			
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+		
+	
