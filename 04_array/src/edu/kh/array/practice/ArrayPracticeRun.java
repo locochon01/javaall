ad3cc94a39f81e59c580f2f72af46cb3c355e375
JungOh <twelve727@gmail.com>
2022-12-14 11:56:32 +0900

    ArrayPractice

---
+++ 04_array/src/edu/kh/array/practice/ArrayPracticeRun.java
@@ -0,0 +1,13 @@
+package edu.kh.array.practice;
+
+public class ArrayPracticeRun {
+
+	public static void main(String[] args) {
+		
+		ArrayPractice arrEx = new ArrayPractice();
+				arrEx.practice4();
+	}
+}
+		
+		//ArrayExample2 arrEx2 = new ArrayExample2();
+				//arrEx2.createLotoNumber();
